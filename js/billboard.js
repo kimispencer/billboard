35ce156483cd2246cf84137396c7c37ee419dec9
kimi spencer <kimispencer@gmail.com>
2014-07-21 14:51:25 -0400

    cleaning up

--- js/billboard.js
+++ js/billboard.js
@@ -3,8 +3,7 @@
 	* 16 July 2014
 */
 
-var allLyrics = [];
-var wordFreq;
+var data;
 var radians = 0.0174532925, // one degree
 	circleRadius = 500,
 	margin = 200,
@@ -28,70 +27,91 @@ var face = d3.select(".chart").append("g")
 // load the data set (defined by user on front-end, such as by decade, by artist, genre, etc.) 
 d3.json("data/test.json", function(error, raw) {
 	processData(raw);
-	visualizeData(wordFreq);
-	foo();
+	visualizeData(data);
 });
 
-function processData(data) {
+function processData(raw) {
+	// console.log(raw[0])
+	var allLyrics = [],
+		allRelationships = [];
 	// 1. collect lyrics from defined sample and join them
-	data.forEach(function(d) {
-		var lyrics = d.lyrics;
-		allLyrics.push(lyrics);
+	raw.forEach(function(d) {
+		allLyrics.push(d.lyrics);
+
+		d.relationships.forEach(function(r) {
+			r.forEach(function(rWord) {
+				console.log(rWord)
+				allRelationships.push(rWord);
+			})
+		})
 	});
 
 	// 2. determine frequency of words within that sample set
-	wordFreq = getWordFrequency(allLyrics.join(" "));
+	data = getWordFrequency(allLyrics.join(" "));	
+
+	data.forEach(function(d) {
+		// console.log(d.text)
+	});
+	// !!! OK PROBLEM, WE JUST CHANGE THE DATA SET SO EACH OBJECT IS AN INDIVIUDAL "WORD"
+	// HOW DO WE ASSIGN INDIVIDUAL RELATIONSHIPS FOR EACH WORD... (AS THEY APPEAR BY...?)
+
+	// raw.forEach(function(d, i) {
+		// console.log(d.relationships)
+		// data[i].relationships = d.relationships;
+		// if(key==="theta") {
+			// console.log(d[key])
+		// }
+	// });
+	// console.log(data)
 };
 
 function visualizeData(data) {
 	drawCircle(data);
+	drawRelationshipArcs();
 };
 
-function foo() {
+function drawRelationshipArcs() {
 	face.selectAll('g')
 		.on('mousedown', function(d, i) {
-		// this key "ngram" : value "word-value"
-		// find object by key "text" : value "word-value"
-		// get key "theta" : value for that end word
-		// draw the arc...
+			// this key "ngram" : value "word-value"
+			// find object by key "text" : value "word-value"
+			// get key "theta" : value for that end word
+			// draw the arc...
 
-		d3.select(this).append('circle')
-			.attr('cx', circleRadius)
-			.attr('cy', 0)
-			.attr('r', 20)
-			.attr('fill', 'green')
+	       d3.keys(d).forEach(function(key) {
+	           	console.log(key + ", " + d[key])
+	           	// if(key==="theta") {
+	           		// console.log(d[key])
+	           	// }
+	        });
 
-       // d3.keys(d).forEach(function(key) {
-          //  	console.log(key + ", " + d[key])
-       //  });
+			// append green circle to group object (which has a rotation applied)
+			d3.select(this).append('circle')
+				.attr('cx', circleRadius)
+				.attr('cy', 0)
+				.attr('r', 20)
+				.attr('fill', 'green')
 
-		var theta = d.theta;
+			var theta = d.theta;
+			// append circle (with rotation) to the clock-face
+			face.append('circle')
+				.attr('cx', circleRadius)
+				.attr('cy', 0)
+				.attr('transform', 'rotate(' + theta + ')')
+				.attr('r', 10)
+				.attr('fill', 'red')
+				// .attr('transform','translate(' + width/2 + ',' + (circleRadius + margin) + ')');
 
-		face.append('circle')
-			.attr('cx', circleRadius)
-			.attr('cy', 0)
-			.attr('transform', 'rotate(' + theta + ')')
-			.attr('r', 10)
-			.attr('fill', 'red')
-			// .attr('transform','translate(' + width/2 + ',' + (circleRadius + margin) + ')');
-
-		// d3.select(this).append('path')
-		// 	.attr('d', 'M' + circleRadius + ',' + 0 + ' Q' + circleRadius + ',' + 0 + ' ' + circleRadius +',' + 0)
-		// 	.attr('class', 'relationship-arc')
-		// 	.transition()
-		// 		.attr('d', 'M' + circleRadius + ',' + 0 + ' Q' + 0 + ',' + 0 + ' ' + startX +',' + startY)
+			// draw bezier curve from start to center to relationship words
+			// d3.select(this).append('path')
+			// 	.attr('d', 'M' + circleRadius + ',' + 0 + ' Q' + circleRadius + ',' + 0 + ' ' + circleRadius +',' + 0)
+			// 	.attr('class', 'relationship-arc')
+			// 	.transition()
+			// 		.attr('d', 'M' + circleRadius + ',' + 0 + ' Q' + 0 + ',' + 0 + ' ' + startX +',' + startY)
 	});
 }
 
 function drawCircle(data) {
-
-	// 0, 0 is center of clock-face
-	// face.append('circle')
-	// 	.attr('cx', 0)
-	// 	.attr('cy', 0)
-	// 	.attr('r', 10)
-	// 	.attr('stroke', 'white');
-
 	var circleScale = d3.scale.linear()
 		.range([0, data.length])
 		.domain([0, 2 * Math.PI]);
@@ -100,67 +120,16 @@ function drawCircle(data) {
 	var bar = face.selectAll('g')
 		.data(data)
 		.enter().append('g')
-		// ROTATION TRANSFORMATION (based on index value)
 		.attr('transform', function(d, i) {
-			// console.log(circleScale(i))
 			return 'rotate('  + circleScale(i) + ')';
 		})
-		// .on('mouseover', function(d, i) {
 		.each(function(d, i) {
 			// set theta
 			d.theta = circleScale(i);
-			// console.log(d);
-
-			// CONVERT THETA INTO X/Y COORDS
-			var theta = d.theta;
-			var startX = circleRadius * Math.cos(theta);
-			var startY = circleRadius * Math.sin(theta);
-			// console.log(startX + ", " + startY)
-
-			// face.append('circle')
-			// 	.attr('cx', startX)
-			// 	.attr('cy', startY)
-			// 	.attr('r', 2)
-			// 	.attr('fill', 'orange')
-
-			// !!! NOTES
-			// data[i] = d 	// data array can be accessed
-			// console.log(d) // data object {text: "we", frequency: 22}
-			// console.log(this)	// html markup
-			// console.log(d3.select(this)) // array... d3 object?
+			// var theta = d.theta;
+			// var x = circleRadius * Math.cos(theta);
+			// var y = circleRadius * Math.sin(theta);
 		})
-		// .on('mousedown', function(d, i) {
-		// 	// this key "ngram" : value "word-value"
-		// 	// find object by key "text" : value "word-value"
-		// 	// get key "theta" : value for that end word
-		// 	// draw the arc...
-
-		// 	// console.log(d)
-		// 	// CONVERT THETA INTO X/Y COORDS
-		// 	var theta = d.theta;
-		// 	var startX = circleRadius * Math.cos(theta);
-		// 	var startY = circleRadius * Math.sin(theta);
-		// 	console.log(startX + ", " + startY)
-
-		// 	face.append('circle')
-		// 		.attr('cx', startX)
-		// 		.attr('cy', startY)
-		// 		.attr('r', 20)
-		// 		.attr('fill', 'red')
-
-  //          // d3.keys(d).forEach(function(key) {
-	 //          //  	console.log(key + ", " + d[key])
-  //          //  });
-
-		// 	// d3.select(this).append('line')
-		// 	// 	.attr('x1', circleRadius-10)
-		// 	// 	.attr('x2', circleRadius)
-		// 	// 	.attr('class', 'relationship-arc')
-		// 	// 	// final destination
-		// 	// 	.transition()
-		// 	// 		.attr('x2', 0)
-		// 	// 		.attr('y2', 0);
-		// });
 		
 	// line
 	bar.append('line')
