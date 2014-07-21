ada4f0db337ad16dfd4e2a63d3b0372f0dfbb6ca
kimi spencer <kimispencer@gmail.com>
2014-07-21 13:04:59 -0400

    finally figuring out rotation...

--- js/billboard.js
+++ js/billboard.js
@@ -3,6 +3,7 @@
 	* 16 July 2014
 */
 
+var allLyrics = [];
 var wordFreq;
 var radians = 0.0174532925, // one degree
 	circleRadius = 500,
@@ -17,20 +18,22 @@ var radians = 0.0174532925, // one degree
 var chart = d3.select(".chart")
 	.attr('height', height*2)
     .attr("preserveAspectRatio", "xMinYMin meet");
+	// !!! resize stuff... not quiet right tho !!!
+	chart.attr("viewBox", "0 0 " + width + " " + width);
 // clock face
 var face = d3.select(".chart").append("g")
 	.attr("class", "clock-face")
 	.attr('transform','translate(' + width/2 + ',' + (circleRadius + margin) + ')');
 
 // load the data set (defined by user on front-end, such as by decade, by artist, genre, etc.) 
-d3.json("data/test.json", function(error, data) {
-	processData(data);
-	visualizeData(wordFreq)
+d3.json("data/test.json", function(error, raw) {
+	processData(raw);
+	visualizeData(wordFreq);
+	foo();
 });
 
 function processData(data) {
 	// 1. collect lyrics from defined sample and join them
-	var allLyrics = [];
 	data.forEach(function(d) {
 		var lyrics = d.lyrics;
 		allLyrics.push(lyrics);
@@ -41,143 +44,54 @@ function processData(data) {
 };
 
 function visualizeData(data) {
-	// !!! resize stuff... not quiet right tho !!!
-	chart.attr("viewBox", function(d) { 
-		return "0 0 " + width + " " + width;
-	});
-	// drawLine(data);
 	drawCircle(data);
-	// drawChordDiagram(data);
-	drawRelationshipArcs(data);
 };
 
-var make_point = function(element, xy)
-{
-    // console.log(element);
-    //var matrix = element.getCTM();
-    //var matrix = element.getScreenCTM();
-    var p = element.nearestViewportElement.createSVGPoint();
-    var matrix = element.getTransformToElement(element.nearestViewportElement);
-    p.x = xy[0];
-    p.y = xy[1];
-    var sp = p.matrixTransform(matrix);
-    return sp;
+function foo() {
+	face.selectAll('g')
+		.on('mousedown', function(d, i) {
+		// this key "ngram" : value "word-value"
+		// find object by key "text" : value "word-value"
+		// get key "theta" : value for that end word
+		// draw the arc...
+
+		d3.select(this).append('circle')
+			.attr('cx', circleRadius)
+			.attr('cy', 0)
+			.attr('r', 20)
+			.attr('fill', 'green')
+
+       // d3.keys(d).forEach(function(key) {
+          //  	console.log(key + ", " + d[key])
+       //  });
+
+		var theta = d.theta;
+
+		d3.select(this).append('circle')
+			.attr('cx', circleRadius)
+			.attr('cy', 0)
+			.attr('transform', 'rotation(' + theta + ')')
+			.attr('r', 5)
+			.attr('fill', 'red')
+			// .attr('transform','translate(' + width/2 + ',' + (circleRadius + margin) + ')');
+
+		// d3.select(this).append('path')
+		// 	.attr('d', 'M' + circleRadius + ',' + 0 + ' Q' + circleRadius + ',' + 0 + ' ' + circleRadius +',' + 0)
+		// 	.attr('class', 'relationship-arc')
+		// 	.transition()
+		// 		.attr('d', 'M' + circleRadius + ',' + 0 + ' Q' + 0 + ',' + 0 + ' ' + startX +',' + startY)
+	});
 }
 
-function drawRelationshipArcs(data) {
-	var center = {x: 0, y: 0};
-		// p1 = {x: 450, y: -200},
-		// p2 = {x: 80, y: 500};
-
-	face.append("circle")
-	    .style("stroke", "white")
-	    .attr("r", 10)
-	    .attr("cx", center.x)
-	    .attr("cy", center.y);
-
-	// var curve = face.append('path')
-	// 	.attr('d', 'M' + p1.x + ',' + p1.y + 'Q' + center.x + ',' + center.y + ' ' + p2.x +',' + p2.y)
-	// 	.attr('class', 'relationship-arc');
-
-	// 	curve.transition()
-	// 		.attr('d', 'M' + p1.x + ',' + p1.y + 'Q' + center.x + ',' + center.y + ' ' + p2.x +',' + p2.y);
-
-/*
-	var districts = chart.selectAll("g");
-
-	var district_centers = districts[0].map(function(d, i) {
-	    var bbox = this.getBBox();
-	    return [bbox.left + bbox.width/2, bbox.top + bbox.height/2];
-	});
-
-	chart
-	  .selectAll("g")
-	  .data(district_centers)
-	  .enter()
-	  .append("circle")
-	     .attr("class", "district_circle")
-	     .attr("cx", function(d){ return d[0]})
-	     .attr("cy", function(d){ return d[1]})
-	     .attr("r", 10)
-	     .attr("fill", "red");
-*/
-
- 	var wordElements = face.selectAll('text');
-	var face2 = d3.select(".chart").append("g")
-		.attr("class", "dot-face")
-
- 	wordElements[0].map(function(d, i) {
- 		var element = d;
- 		console.log(element.getBoundingClientRect())
- 		var foo = element.getBoundingClientRect();
-
-		var box = face2.append('rect')
-			.attr('x', foo.top) //p.x)
-			.attr('y', foo.left) //p.y)
-			.attr('width', foo.width) //bbox.width)
-			.attr('height', foo.height) //bbox.height)
-			.attr('stroke', 'red')
-			.attr('stroke-width', '2px')
-			.attr('fill', 'none')
- 	});
-
-	// var node = wordElements.node();
-	// var bbox = node.getBBox();
-	// var topLeft = make_point(node, [bbox.x, bbox.y]);
-
-	// var b = chart.append('rect')
-	// 	.attr('x', topLeft.x)
-	// 	.attr('y', topLeft.y)
-	// 	.attr('width', bbox.width)
-	// 	.attr('height', bbox.height)
-	// 	.attr('stroke', 'red')
-	// 	.attr('stroke-width', '2px')
-	// 	.attr('fill', 'none')
-
-	// for(var i=0; i < wordElements.length; i++) {
-	// 	var wordElem = wordElements[1];
-	// 	console.log(wordElem)
-	// 	var node = wordElem.node();
-	// 	var bbox = node.getBBox();
-	// 	var topLeft = make_point(node, [bbox.x, bbox.y]);
-
-	// 	var b = chart.append('rect')
-	// 		.attr('x', topLeft.x)
-	// 		.attr('y', topLeft.y)
-	// 		.attr('width', bbox.width)
-	// 		.attr('height', bbox.height)
-	// 		.attr('stroke', 'red')
-	// 		.attr('stroke-width', '2px')
-	// 		.attr('fill', 'none')
-	// }
-};
-
-function drawLine(data) {
-	var linearScale = d3.scale.linear()
-		.range([0, data.length])
-		.domain([0, 40]);
-
-	var ordinalScale = d3.scale.ordinal()
-	    .rangeRoundBands([0, width], 0.1)
-	    .domain(data.map(function(d) { return d.letter; }));
-
-	// chart.style('width', function(d) { return linearScale(d) + 'px'; })
-
-	var bar = chart.selectAll('g')
-		.data(data)
-		.enter().append('g');
-
-	bar.append('text')
-		.text(function(d) {
-			return d.text;
-		})
-		.attr('transform', function(d, i) {
-			return "translate(" + linearScale(i) + ", 100) rotate(270)";
-		})
-		.attr('class', 'word');
-};
-
 function drawCircle(data) {
+
+	// 0, 0 is center of clock-face
+	// face.append('circle')
+	// 	.attr('cx', 0)
+	// 	.attr('cy', 0)
+	// 	.attr('r', 10)
+	// 	.attr('stroke', 'white');
+
 	var circleScale = d3.scale.linear()
 		.range([0, data.length])
 		.domain([0, 2 * Math.PI]);
@@ -186,25 +100,81 @@ function drawCircle(data) {
 	var bar = face.selectAll('g')
 		.data(data)
 		.enter().append('g')
-		// .on('mouseover', function(d) {
-		// 	var word = d3.select(this)
-		// 	console.log(word)
+		// ROTATION TRANSFORMATION (based on index value)
+		.attr('transform', function(d, i) {
+			// console.log(circleScale(i))
+			return 'rotate('  + circleScale(i) + ')';
+		})
+		// .on('mouseover', function(d, i) {
+		.each(function(d, i) {
+			// set theta
+			d.theta = circleScale(i);
+			// console.log(d);
+
+			// CONVERT THETA INTO X/Y COORDS
+			var theta = d.theta;
+			var startX = circleRadius * Math.cos(theta);
+			var startY = circleRadius * Math.sin(theta);
+			// console.log(startX + ", " + startY)
+
+			// face.append('circle')
+			// 	.attr('cx', startX)
+			// 	.attr('cy', startY)
+			// 	.attr('r', 2)
+			// 	.attr('fill', 'orange')
+
+			// !!! NOTES
+			// data[i] = d 	// data array can be accessed
+			// console.log(d) // data object {text: "we", frequency: 22}
+			// console.log(this)	// html markup
+			// console.log(d3.select(this)) // array... d3 object?
+		})
+		// .on('mousedown', function(d, i) {
+		// 	// this key "ngram" : value "word-value"
+		// 	// find object by key "text" : value "word-value"
+		// 	// get key "theta" : value for that end word
+		// 	// draw the arc...
+
+		// 	// console.log(d)
+		// 	// CONVERT THETA INTO X/Y COORDS
+		// 	var theta = d.theta;
+		// 	var startX = circleRadius * Math.cos(theta);
+		// 	var startY = circleRadius * Math.sin(theta);
+		// 	console.log(startX + ", " + startY)
+
+		// 	face.append('circle')
+		// 		.attr('cx', startX)
+		// 		.attr('cy', startY)
+		// 		.attr('r', 20)
+		// 		.attr('fill', 'red')
+
+  //          // d3.keys(d).forEach(function(key) {
+	 //          //  	console.log(key + ", " + d[key])
+  //          //  });
+
+		// 	// d3.select(this).append('line')
+		// 	// 	.attr('x1', circleRadius-10)
+		// 	// 	.attr('x2', circleRadius)
+		// 	// 	.attr('class', 'relationship-arc')
+		// 	// 	// final destination
+		// 	// 	.transition()
+		// 	// 		.attr('x2', 0)
+		// 	// 		.attr('y2', 0);
 		// });
 		
-	// bar.append('line')
-	// 	.attr('y1',circleRadius)
-	// 	.attr('y2',circleRadius + tickLength)
-	// 	.attr('class', 'tick');
+	// line
+	bar.append('line')
+		.attr('x1',circleRadius)
+		.attr('x2',circleRadius + tickLength)	
+		.attr('class', 'tick');
 
+	// text
 	bar.append('text')
 		.text(function(d){
 			return d.text;
 		})
 		.attr('x', function(d) {
 			return circleRadius;
-		})
-		.attr('transform', function(d, i){
-			return 'rotate('  + circleScale(i) + ')';
 		})
 		.attr('font-size', function(d) {
 			return d.frequency * 5;
@@ -216,19 +186,7 @@ function drawCircle(data) {
 			var word = d3.select(this).attr('dy', 0);
 		})
 		.attr('class', 'word');
-	};
-
-// other visualization stuff:
-// 3. determine frequency of correlations between words in that sample set
-// later: sentiment analysis (color), bpm (audio?), etc.
-
-// front-end functionalities:
-// 1. define sample set (individual artist, year, decade, or genre)
-// 2. transitions / animations (linear to circular)
-
-
-
-
+};
 
 
 
--- js/chord.js
+++ js/chord.js
@@ -1,3 +1,165 @@
+
+// var make_point = function(element, xy)
+// {
+//     // console.log(element);
+//     //var matrix = element.getCTM();
+//     //var matrix = element.getScreenCTM();
+//     var p = element.nearestViewportElement.createSVGPoint();
+//     var matrix = element.getTransformToElement(element.nearestViewportElement);
+//     p.x = xy[0];
+//     p.y = xy[1];
+//     var sp = p.matrixTransform(matrix);
+//     return sp;
+// }
+
+// var center = {x: width/2, y: width/2};
+// 	p1 = {x: 450, y: -200},
+// 	p2 = {x: 10, y: circleRadius};
+
+// function drawRelationshipArcs(data) {
+	// face.append("circle")
+	//     .style("stroke", "red")
+	//     .attr('fill', 'grey')
+	//     .attr("r", circleRadius)
+	//     .attr("cx", 0)	// 0, 0 is center appeneded to "face"
+	//     .attr("cy", 0);
+
+ // 	var wordElements = face.selectAll('text');
+	// var face2 = d3.select(".chart").append("g")
+	// 	.attr("class", "dot-face")
+
+	// d3.selectAll(wordElements).each(function(d, i) {
+		// console.log(d, i, wordElements[i]);
+		// console.log(wordElements[i])
+ 	// 	var element = wordElements[i].node();
+ 	// 	// console.log(element.getBoundingClientRect())
+ 	// 	var foo = element.getBoundingClientRect();
+ 	// 	// console.log(foo)
+
+		// var box = face2.append('rect')
+		// 	.attr('x', foo.top) //p.x)
+		// 	.attr('y', foo.left) //p.y)
+		// 	.attr('width', foo.width) //bbox.width)
+		// 	.attr('height', foo.height) //bbox.height)
+		// 	.attr('stroke', 'red')
+		// 	.attr('stroke-width', '2px')
+		// 	.attr('fill', 'none')
+	// });
+
+ 	// wordElements[0].map(function(d, i) {
+ 	// 	var element = d;
+ 	// 	// console.log(element[0][0].getBoundingClientRect())
+ 	// 	var foo = element.getBoundingClientRect();
+ 	// 	// console.log(foo)
+
+		// var box = face2.append('rect')
+		// 	.attr('x', foo.top) //p.x)
+		// 	.attr('y', foo.left) //p.y)
+		// 	.attr('width', foo.width) //bbox.width)
+		// 	.attr('height', foo.height) //bbox.height)
+		// 	// .attr('stroke', 'red')
+		// 	.attr('stroke-width', '1px')
+		// 	.attr('fill', 'none')
+
+		// // var line = face2.append('line')
+		// // 	.attr('x1', foo.top)
+		// // 	.attr('y1', foo.left)
+		// // 	.attr('x2', 0)
+		// // 	.attr('y2', 0)
+
+		// var curve = face2.append('path')
+		// 	.attr('d', 'M' + foo.top + ',' + foo.left + ' Q' + center.x + ',' + center.y + ' ' + p2.x +',' + p2.y)
+		// 	.attr('class', 'relationship-arc');
+
+		// 	// curve.transition()
+		// 	// 	.attr('d', 'M' + p1.x + ',' + p1.y + 'Q' + center.x + ',' + center.y + ' ' + p2.x +',' + p2.y);
+ 	// });
+
+	// var node = wordElements.node();
+	// var bbox = node.getBBox();
+	// var topLeft = make_point(node, [bbox.x, bbox.y]);
+
+	// var b = chart.append('rect')
+	// 	.attr('x', topLeft.x)
+	// 	.attr('y', topLeft.y)
+	// 	.attr('width', bbox.width)
+	// 	.attr('height', bbox.height)
+	// 	.attr('stroke', 'red')
+	// 	.attr('stroke-width', '2px')
+	// 	.attr('fill', 'none')
+
+	// for(var i=0; i < wordElements.length; i++) {
+	// 	var wordElem = wordElements[1];
+	// 	console.log(wordElem)
+	// 	var node = wordElem.node();
+	// 	var bbox = node.getBBox();
+	// 	var topLeft = make_point(node, [bbox.x, bbox.y]);
+
+	// 	var b = chart.append('rect')
+	// 		.attr('x', topLeft.x)
+	// 		.attr('y', topLeft.y)
+	// 		.attr('width', bbox.width)
+	// 		.attr('height', bbox.height)
+	// 		.attr('stroke', 'red')
+	// 		.attr('stroke-width', '2px')
+	// 		.attr('fill', 'none')
+	// }
+// };
+
+// function drawLine(data) {
+// 	var linearScale = d3.scale.linear()
+// 		.range([0, data.length])
+// 		.domain([0, 40]);
+
+// 	var ordinalScale = d3.scale.ordinal()
+// 	    .rangeRoundBands([0, width], 0.1)
+// 	    .domain(data.map(function(d) { return d.letter; }));
+
+// 	// chart.style('width', function(d) { return linearScale(d) + 'px'; })
+
+// 	var bar = chart.selectAll('g')
+// 		.data(data)
+// 		.enter().append('g');
+
+// 	bar.append('text')
+// 		.text(function(d) {
+// 			return d.text;
+// 		})
+// 		.attr('transform', function(d, i) {
+// 			return "translate(" + linearScale(i) + ", 100) rotate(270)";
+// 		})
+// 		.attr('class', 'word');
+// };
+
+// var make_arc = function(element) {
+// 	console.log(element)
+// 	var theta = Math.atan2(element.left, element.top) * (180 / Math.PI);
+// 	console.log(theta)
+// 	var curve = face.append('path')
+// 		.attr('d', 'M' + element.top + ',' + circleRadius + 'Q' + center.x + ',' + center.y + ' ' + p2.x +',' + p2.y)
+// 		.attr('class', 'relationship-arc');
+
+// 	var b = face.append('rect')
+// 		.attr('x', element.top)
+// 		.attr('y', element.left)
+// 		.attr('width', 10)
+// 		.attr('height', 10)
+// 		.attr('stroke', 'red')
+// 		.attr('stroke-width', '2px')
+// 		.attr('fill', 'none')
+// };
+
+// other visualization stuff:
+// 3. determine frequency of correlations between words in that sample set
+// later: sentiment analysis (color), bpm (audio?), etc.
+
+// front-end functionalities:
+// 1. define sample set (individual artist, year, decade, or genre)
+// 2. transitions / animations (linear to circular)
+
+
+
+
 // // From http://mkweb.bcgsc.ca/circos/guide/tables/
 // var chord = d3.layout.chord()
 //   .padding(.05)
